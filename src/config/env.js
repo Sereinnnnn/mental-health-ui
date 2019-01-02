d71742dac4bea953a46164fbca72012d75e2f09f
tangyi <1633736729@qq.com>
2019-01-02 20:31:08 +0800

    优化

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,7 @@
 let baseUrl
+let iconfontVersion = ['567566_r22zi6t8noas8aor', '599693_0b5sleso3f1j1yvi', '667895_xte3dcfrvbo6r'];
+let iconfontUrl = `//at.alicdn.com/t/font_$key.css`;
+let codeUrl = `/admin/code`
 
 if (process.env.NODE_ENV === 'development') {
   baseUrl = `http://127.0.0.1:9999/`
@@ -7,5 +10,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export {
-  baseUrl
+  baseUrl,
+  iconfontUrl,
+  iconfontVersion,
+  codeUrl
 }
